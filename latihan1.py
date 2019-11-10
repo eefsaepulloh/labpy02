71c3075b6591e1cfb4fdf392d5870a7ae8091436
eefsaepulloh <56913656+eefsaepulloh@users.noreply.github.com>
2019-11-10 14:44:30 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,19 @@
+a = int( input ( "Masukkan bilangan A: " ))
+
+b = int( input ( "Masukkan bilangan B: " ))
+
+c = int( input ( "Masukkan bilangan C: " ))
+
+
+
+if a>b and a>c:
+
+    print("A lebih besar dari B dan c")
+
+elif a<b and b>c:
+
+     print("B lebih besar dari A dan C")
+
+else:
+
+     print ("C lebih besar dari A dan B")
